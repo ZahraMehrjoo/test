73d0e692fb074f3042debe20fea8b17384646bb3
ZahraMehrjoo <z.mehrjoo73@yahoo.com>
2016-02-08 14:10:01 +0330

    main

---
+++ GccBoardProject7/GccBoardProject7/src/main.c
@@ -0,0 +1,8 @@
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+
+	/* Insert application code here, after the board has been initialized. */
+}
